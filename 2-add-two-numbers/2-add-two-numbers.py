facdbeb4f422a628992a6be9cb9876c99a44b38d
nuredinbedruyimer <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-01-16 08:47:32 -0800

    Time: 59 ms (97.92%), Space: 13.8 MB (99.39%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        adder=ListNode(val=(l1.val+l2.val)%10)
+        carry=(l1.val+l2.val)//10
+        current_node=adder
+        while l1.next and l2.next:
+            l1=l1.next
+            l2=l2.next
+            current_node.next=ListNode(val=(carry+l1.val+l2.val)%10)
+            carry=(carry+l1.val+l2.val)//10
+            current_node=current_node.next
+        while(l1.next):
+            l1=l1.next
+            current_node.next=ListNode(val=(carry+l1.val)%10)
+            carry=(carry+l1.val)//10
+            current_node=current_node.next
+        while(l2.next):
+            l2=l2.next
+            current_node.next=ListNode(val=(carry+l2.val)%10)
+            carry=(carry+l2.val)//10
+            current_node=current_node.next
+        if carry>0:
+            current_node.next=ListNode(val=1)
+            
+        return (adder)
+            
