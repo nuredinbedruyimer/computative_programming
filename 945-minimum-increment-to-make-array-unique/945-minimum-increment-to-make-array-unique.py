b67e1adaed62fd05396e2fb4aee12c205b112612
nuredinbedruyimer <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-01-17 03:12:52 -0800

    Time: 866 ms (91.10%), Space: 28.3 MB (54.83%) - LeetHub

---
+++ 945-minimum-increment-to-make-array-unique/945-minimum-increment-to-make-array-unique.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minIncrementForUnique(self, nums: List[int]) -> int:
+        # solution one
+        nums.sort()
+        ans,preValue=0,nums[0]
+        length=len(nums)
+        for index in range(1,length):
+            currentValue=nums[index]
+            if currentValue<=preValue:
+                ans +=preValue-currentValue+1
+                currentValue=preValue+1
+            preValue=currentValue
+        return ans
+
+        
