8ed1cb0f86920c521895b70c5a1972967144dfee
nuredinbedruyimer <119440292+nuredinbedruyimer@users.noreply.github.com>
2023-01-29 15:00:47 -0800

    Time: 596 ms (87.68%), Space: 14.7 MB (54.18%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.py
@@ -2,8 +2,8 @@ class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
         maxOnes=0
         left=0
-        for right,num in enumerate(nums):
-            k=k-1+num # binary number
+        for right in range(len(nums)):
+            k=k-1+nums[right] # binary number
             if(k<0):
                 k+=1-nums[left]
                 left=left+1
