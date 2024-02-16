6861739e1ad55ae739e1434f6b713db1e34c9680
Nuredin Bedru <119440292+nuredinbedruyimer@users.noreply.github.com>
2024-02-16 11:09:51 -0800

    Add solution for Remove Element

--- camp2/leetcode/remove-element.py
+++ camp2/leetcode/remove-element.py
@@ -1,16 +1,12 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        pointer=0
-        for right in range(len(nums)):
-            if nums[right]!=val:
-                nums[pointer]=nums[right]
-                pointer=pointer+1
-            print(nums)
-        
-                
-                
-                
-            
-        return pointer
+        def get_answer():
+            left=0
+            for right in range(len(nums)):
+                if nums[right]!=val:
+                    nums[left]=nums[right]
+                    left=left+1
+            return left
+        return get_answer()
         
         
