59b5a39f807f1d84868acccb0964045352568c9b
Nuredin Bedru <119440292+nuredinbedruyimer@users.noreply.github.com>
2024-02-17 23:10:39 -0800

    Add solution for Rotate Image

---
+++ camp2/leetcode/rotate-image.py
@@ -0,0 +1,32 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+
+        """
+
+        """
+        1 2 3 ----swap the row    7 8 9 ---->then do transponse of it 7 4 1 
+        4 5 6                     4 5 6                               8 5 2
+        7 8 9                     1 2 3                               9 6 3
+
+        if n is odd no need to swap the last row then using unmeeted pointer
+        
+        """
+        def get_answer():
+            N = len(matrix)
+            M = len(matrix[0])
+            # reverse the matrix in row
+            left = 0
+            right = N-1
+            while left<right:
+                matrix[left], matrix[right] = matrix[right], matrix[left]
+                left+=1
+                right-=1
+            for row in range(N):
+                for column in range(row):
+                    matrix [row][column],matrix[column][row]=matrix[column][row], matrix [row][column]
+        get_answer()
+        
+
+        
