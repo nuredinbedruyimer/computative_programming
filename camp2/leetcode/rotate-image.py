9fafcab7f2478d5489fcfb5003fb70061b36f6e7
Nuredin Bedru <119440292+nuredinbedruyimer@users.noreply.github.com>
2024-02-22 08:13:03 -0800

    Add solution for Rotate Image

--- camp2/leetcode/rotate-image.py
+++ camp2/leetcode/rotate-image.py
@@ -13,18 +13,23 @@ class Solution:
         if n is odd no need to swap the last row then using unmeeted pointer
         
         """
-        def get_answer():
-            N = len(matrix)
-            M = len(matrix[0])
-            # reverse the matrix in row
+        def reverse(N):
             left = 0
             right = N-1
             while left<right:
                 matrix[left], matrix[right] = matrix[right], matrix[left]
                 left+=1
                 right-=1
+            
+        def get_answer():
+            N = len(matrix)
+            M = len(matrix[0])
+            # reverse the matrix in row
+            reverse(N)
+           
             for row in range(N):
                 for column in range(row):
+                    #  swapping
                     matrix [row][column],matrix[column][row]=matrix[column][row], matrix [row][column]
         get_answer()
         
