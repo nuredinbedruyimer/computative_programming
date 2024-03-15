4824a2c9e094d80b1cf0f720fe76b0600c6dbb44
Nuredin Bedru <119440292+nuredinbedruyimer@users.noreply.github.com>
2024-03-15 13:05:14 -0700

    Add solution for Interesting drink

---
+++ camp2/codeforces/706B Interesting-drink.py
@@ -0,0 +1,33 @@
+def get_left(arr, target):
+
+    low = -1
+
+    high = len(arr)
+
+    while high > low + 1 :
+
+        middle = low + (high - low) // 2
+
+        if arr[middle] > target:
+            
+            high = middle
+        else:
+            
+            low = middle
+    
+    return low
+
+
+
+test = int(input())
+
+arr = list(map(int, input().split()))
+
+arr.sort()
+
+queries = int(input())
+
+for _ in range(queries):
+    curr = int(input())
+
+    print(get_left(arr, curr)+1)
